ddc67d9103607527564a05e9c68c91ffc7bfdc42
Chumplumps <47583539+Chumplumps@users.noreply.github.com>
2019-11-30 17:04:09 +1100

    Initial commit

---
+++ Assets/Shaders/Fog.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Fog : MonoBehaviour
+{
+    public Material effectMaterial;
+
+    void Start()
+    {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, effectMaterial);
+    }
+}
---
+++ Assets/Shaders/Pixelate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Pixelate : MonoBehaviour
+{
+    public Material effectMaterial;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, effectMaterial);
+    }
+}
